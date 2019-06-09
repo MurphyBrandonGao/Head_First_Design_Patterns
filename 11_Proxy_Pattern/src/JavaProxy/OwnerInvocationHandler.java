7e879ded0d5150c345ee1b1c6c3e4f206a3e077c
GaoBo <18279552824@163.com>
2019-06-09 23:08:22 +0800

    add proxy pattern

---
+++ 11_Proxy_Pattern/src/JavaProxy/OwnerInvocationHandler.java
@@ -0,0 +1,35 @@
+package JavaProxy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * @author Dell
+ * @create 2019-06-01 10:31
+ */
+public class OwnerInvocationHandler implements InvocationHandler {
+
+    PersonBean person;
+
+    public OwnerInvocationHandler(PersonBean person) {
+        this.person = person;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        try {
+            if (method.getName().startsWith("get")) {
+                return method.invoke(person, args);
+            } else if (method.getName().equals("setHotOrNotRating")) {
+                throw new IllegalAccessException();
+            } else if (method.getName().startsWith("set")) {
+                return method.invoke(person, args);
+            }
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
---
+++ 11_Proxy_Pattern/src/JavaProxy/PersonBean.java
@@ -0,0 +1,17 @@
+package JavaProxy;
+
+/**
+ * @author Dell
+ * @create 2019-06-01 10:23
+ */
+public interface PersonBean {
+    String getName();
+    String getGender();
+    String getInterests();
+    int getHotOrNotRating();
+
+    void setName(String name);
+    void setGender(String gender);
+    void setInterests(String interests);
+    void setHotOrNotRating(int rating);
+}
